03d9efd1e34318eb804af3efc8045b3f6c9ac4ad
eduardoandd <eduardo.andrade.dev@gmail.com>
2025-04-13 23:09:27 -0300

    finalizando login

--- financas/api/controllers/auth.js
+++ financas/api/controllers/auth.js
@@ -1,5 +1,6 @@
 import {db} from '../connect.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 export const register = (req,res) => {
     const {username, email, password, confirmPassword} = req.body
@@ -44,4 +45,50 @@ export const register = (req,res) => {
 }
  
 
-export const login = (req,res) => {}
+export const login = (req,res) => {
+    const {email, password} = req.body
+
+    db.query(
+        "SELECT * FROM user WHERE email = ?",[email], async(error,data) =>{
+            if(error){
+                console.log(error)
+                return res.status(500).json({msg:"Aconteceu algum erro no servidor, tente novamente mais tarde"})
+            }
+            if(data.length === 0){
+                return res.status(404).json({msg:'Usuário não encontrado!'})
+            }
+            else{
+                const user = data[0]
+
+                const checkPassowrd = await bcrypt.compare(password, user.password)
+
+                if(!checkPassowrd){
+                    console.log("oi")
+                    return res.status(422).json({msg:'Senha incorreta'})
+                }   
+                
+                try {
+                    const refreshToken = jwt.sign({
+                        exp: Math.floor(Date.now()/1000) + 24 * 60 * 60,
+                        id: user.password
+                    },
+                        process.env.REFRESH,
+                        {algorithm:"HS256"}
+                    )
+                    const token = jwt.sign({
+                        exp: Math.floor(Date.now()/1000) + 3600,
+                        id: user.password
+                    },
+                        process.env.TOKEN,
+                        {algorithm:"HS256"}
+                    )
+                    res.status(200).json({msg:"Usuário logado com sucesso!", token, refreshToken})
+                } catch (error) {
+                    console.log(error)
+                    return res.status().json({msg:"Aconteceu algum erro no servidor, tente novamente mais tarde"})
+                }
+            }
+        }
+    )
+
+}
